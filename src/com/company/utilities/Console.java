91c7e70affe7208f0c3ce8f1fba5ffc53ac0220d
Matthew Greco <mgreco2112@tutanota.com>
2021-10-23 07:40:16 -0400

    created getInt and getString methods

---
+++ src/com/company/utilities/Console.java
@@ -0,0 +1,43 @@
+package com.company.utilities;
+
+import java.util.Scanner;
+
+public class Console {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int getInt(String prompt, int min, int max) {
+        int output;
+
+        do {
+            System.out.println(prompt);
+
+            String input = scanner.nextLine();
+
+            try {
+                output = Integer.parseInt(input);
+            } catch (Exception e) {
+                System.out.println("Invalid entry, try again\n");
+
+                output = min - 1;
+            }
+        } while (output >= min || output <= max);
+
+        return output;
+    }
+
+    public static String getString(String prompt, boolean isRequired) {
+        String output = "";
+
+        System.out.println(prompt);
+
+        output = scanner.nextLine();
+
+        if (output.equals("") && isRequired) {
+            System.out.println("Input is required to proceed");
+            getString(prompt, true);
+        }
+
+        return output;
+    }
+
+}
